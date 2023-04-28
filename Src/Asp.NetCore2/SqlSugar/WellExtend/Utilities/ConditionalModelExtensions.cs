e9441c65170a47faafc05e805a40e194785376ac
jiangfukang <fukang.jiang@wellthinic.com>
2023-04-28 10:31:05 +0800

    Create ConditionalModelExtensions.cs

---
+++ Src/Asp.NetCore2/SqlSugar/WellExtend/Utilities/ConditionalModelExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlSugar
+{
+    public static class ConditionalModelExtensions
+    {
+        public static void Add(this List<IConditionalModel> conditions, string fieldName, object fieldValue, ConditionalType conditionalType = ConditionalType.Equal)
+        {
+            if (fieldValue is bool)
+            {
+                conditions.Add(new ConditionalModel()
+                {
+                    FieldName = fieldName,
+                    FieldValue = fieldValue.ToString()?.ToLower(),
+                    CSharpTypeName = "bool",
+                    ConditionalType = conditionalType
+                });
+            }
+            else
+            {
+                conditions.Add(new ConditionalModel()
+                {
+                    FieldName = fieldName,
+                    FieldValue = fieldValue.ToString(),
+                    ConditionalType = conditionalType
+                });
+            }
+        }
+
+    }
+
+    public static class SugarConditional
+    {
+        public static List<IConditionalModel> Create(string fieldName, object fieldValue, ConditionalType conditionalType = ConditionalType.Equal)
+        {
+            List<IConditionalModel> conditions = new List<IConditionalModel>();
+
+            if (fieldValue is bool)
+            {
+                conditions.Add(new ConditionalModel()
+                {
+                    FieldName = fieldName,
+                    FieldValue = fieldValue.ToString()?.ToLower(),
+                    CSharpTypeName = "bool",
+                    ConditionalType = conditionalType
+                });
+            }
+            else
+            {
+                conditions.Add(new ConditionalModel()
+                {
+                    FieldName = fieldName,
+                    FieldValue = fieldValue.ToString(),
+                    ConditionalType = conditionalType
+                });
+            }
+
+            return conditions;
+        }
+
+    }
+}
