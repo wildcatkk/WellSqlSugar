a2e921957e468b6ca911b2a39b5c840001d556d5
jiangfukang <fukang.jiang@wellthinic.com>
2023-04-28 12:12:39 +0800

    Create WellFilter.cs

---
+++ Src/Asp.NetCore2/SqlSugar/WellExtend/WellFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace SqlSugar
+{
+    public partial class QueryableProvider<T> : QueryableAccessory, ISugarQueryable<T>
+    {
+        public ISugarQueryable<T> WellFilter(long factoryId, bool isDeleted = false)
+        {
+            List<IConditionalModel> conditions = new List<IConditionalModel>();
+
+            Type type = typeof(T);
+            if (type.GetInterface(nameof(ILogicalDelete)) != null)
+            {
+                conditions.Add(nameof(ILogicalDelete.IsDeleted), isDeleted);
+            }
+
+            if (factoryId > 0 && type.GetInterface(nameof(IFactory)) != null)
+            {
+                conditions.Add(nameof(IFactory.FactoryId), factoryId);
+            }
+
+            if (conditions.Count > 0)
+            {
+                return this.Where(conditions);
+            }
+            else
+            {
+                return this;
+            }
+        }
+
+    }
+}
