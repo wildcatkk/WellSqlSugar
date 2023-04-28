06c61aaf59d69cdd12ba0103a3f376e33499e768
jiangfukang <fukang.jiang@wellthinic.com>
2023-04-28 12:12:45 +0800

    Create ToList.cs

---
+++ Src/Asp.NetCore2/SqlSugar/WellExtend/ToList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugar
+{
+    public partial class QueryableProvider<T> : QueryableAccessory, ISugarQueryable<T>
+    {
+        public List<T> ToList(long factoryId, bool isDeleted = false)
+        {
+            WellFilter(factoryId, isDeleted);
+
+            //TODO 处理自定义特性
+
+            return this.ToSugarList();
+        }
+
+        public Task<List<T>> ToListAsync(long factoryId, bool isDeleted = false)
+        {
+            WellFilter(factoryId, isDeleted);
+
+            //TODO 处理自定义特性
+
+            return this.ToSugarListAsync();
+        }
+    }
+}
