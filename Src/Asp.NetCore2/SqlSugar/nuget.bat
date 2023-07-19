6bcd9c2fc7b59f39437955d9c74256414b5c3d84
wildcatkk <45650708+wildcatkk@users.noreply.github.com>
2023-07-19 15:59:57 +0800

    版本号处理

--- Src/Asp.NetCore2/SqlSugar/nuget.bat
+++ Src/Asp.NetCore2/SqlSugar/nuget.bat
@@ -2,8 +2,8 @@
 
 dotnet nuget delete WellSqlSugar 5.1.4.67 --non-interactive -k well.123 -s http://123.57.57.116/WellNuGetService/nuget
 
-dotnet nuget push "%~dp0WellSqlSugar.5.1.4.67.nupkg" -k well.123 -s http://123.57.57.116/WellNuGetService/nuget
+dotnet nuget push "%~dp0WellSqlSugar5_1_4_67.1.1.0.nupkg" -k well.123 -s http://123.57.57.116/WellNuGetService/nuget
 
-del /f /s /q "%~dp0WellSqlSugar.5.1.4.67.nupkg"
+del /f /s /q "%~dp0WellSqlSugar5_1_4_67.1.1.0.nupkg"
 
 pause
