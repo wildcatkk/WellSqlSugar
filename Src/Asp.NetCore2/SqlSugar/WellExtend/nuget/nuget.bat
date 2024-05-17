d248e44149b9cd478002466dd78cc52d3e8f9f39
wildcatkk <jiang341204@live.com>
2024-05-17 15:30:46 +0800

    WellSqlSugar5_1_4_154 1.0.0

--- Src/Asp.NetCore2/SqlSugar/WellExtend/nuget/nuget.bat
+++ Src/Asp.NetCore2/SqlSugar/WellExtend/nuget/nuget.bat
@@ -1,11 +1,11 @@
-set version=1.0.9
+set version=1.0.0
 
 dotnet build "../../SqlSugar.csproj" -p:PackageVersion=%version%
 
 %~dp0nuget.exe pack %~dp0SqlSugarForCore.nuspec -OutputDirectory %~dp0
 
-dotnet nuget push "%~dp0WellSqlSugar5_1_4_94.%version%.nupkg" -k well.123 -s http://123.57.57.116/WellNuGetService/nuget
+dotnet nuget push "%~dp0WellSqlSugar5_1_4_154.%version%.nupkg" -k well.123 -s http://123.57.57.116/WellNuGetService/nuget
 
-del /f /s /q "%~dp0WellSqlSugar5_1_4_94.%version%.nupkg"
+del /f /s /q "%~dp0WellSqlSugar5_1_4_154.%version%.nupkg"
 
 pause
